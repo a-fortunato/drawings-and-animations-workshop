73169b152babe254707d7ed97f9b53ec7ab94a77
Anggella <anggella.fortunato@gmail.com>
2022-08-05 14:53:48 +0200

    Revert "Parent fixes"

--- src/Stickers/Stickers.tsx
+++ src/Stickers/Stickers.tsx
@@ -192,6 +192,8 @@ export const Stickers = ({ navigation }) => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
       quality: 1,
     });
 
